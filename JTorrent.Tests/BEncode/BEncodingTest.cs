37aa3ab104a1c2755900f066230f6de08405afc9
Jérémy Bauzone <bauzonejeremy@gmail.com>
2014-05-25 19:42:22 +0200

    Création de la class BEcoding et de son test unitaire associé Ajout de 2 construteurs pour initiliser la class

---
+++ JTorrent.Tests/BEncode/BEncodingTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JTorrent.BEncode;
+using System.IO;
+
+namespace JTorrent.Tests.BEncode {
+
+    /// <summary>
+    /// Tests concernant le décodage du bencode
+    /// </summary>
+    [TestClass]
+    public class BEncodingTest {
+
+        /// <summary>
+        /// Vérification de l'existence du fichier
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException), "The given file was not found.")]
+        public void CheckFileExists() {            
+            BEncoding encoding = new BEncoding("fichierquiexistepas.torrent");
+        }
+
+        /// <summary>
+        /// Vérifie un fichier torrent en extension
+        /// </summary>
+        [TestMethod]
+        public void CheckFileTypeTorrent() {
+
+            try {
+
+                BEncoding encoding = new BEncoding("fichierquiexistepas.torrent");
+                Assert.Fail("An exception should have been thrown");
+            
+            } catch(FileNotFoundException ex) {
+                Assert.AreEqual("The given file was not found.", ex.Message);
+            } catch (Exception ex) {
+                Assert.Fail(string.Format( "Unexpected exception of type {0} caught: {1}", ex.GetType(), ex.Message));
+            }
+        }
+
+        /// <summary>
+        /// Vérification de l'extension du fichier en cas d'un fichier différent de .torrent
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "The given file is not a .torrent file.")]
+        public void CheckFileTypeNotTorrent() {
+            BEncoding encoding = new BEncoding("fichierquiexistepas.extension");
+        }
+        
+        /// <summary>
+        /// Vérification de l'absence de fichier passé
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "filePath argument cannot be null or empty.")]
+        public void CheckFilePathEmpty() {
+            BEncoding encoding = new BEncoding(string.Empty);
+        }
+
+        /// <summary>
+        /// Vérification de la longueur des datas passées (ne peut être égale à 0)
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "data argument cannot have a length of 0.")]
+        public void CheckDataEmpty() {
+            BEncoding encoding = new BEncoding(new byte[0]);
+        }
+    }
+}
---
+++ JTorrent/BEncode/BEncoding.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JTorrent.BEncode {
+
+    /// <summary>
+    /// Classe permettant le décodage d'une donnée BEncode (voir http://en.wikipedia.org/wiki/Bencode)
+    /// </summary>
+    public class BEncoding {
+
+        private byte[] _data;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <exception cref="System.ArgumentNullException"></exception>
+        /// <exception cref="System.ArgumentOutOfRangeException"></exception>
+        /// <exception cref="System.FileNotFoundException"></exception>
+        /// <param name="filePath"></param>
+        public BEncoding(string filePath) {
+
+            if (string.IsNullOrEmpty(filePath))
+                throw new ArgumentNullException("filePath", "filePath argument cannot be null or empty.");
+
+            if (!Path.GetExtension(filePath).Equals(".torrent"))
+                throw new ArgumentOutOfRangeException("filePath", "The given file is not a .torrent file.");
+
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException("The given file was not found.", filePath);
+
+            //on récupère tout le fichier sous forme de bytes
+            _data = File.ReadAllBytes(filePath);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <exception cref="System.ArgumentNullException"></exception>
+        /// <exception cref="System.ArgumentException"></exception>
+        /// <param name="data"></param>
+        public BEncoding(byte[] data) {
+            
+            //data ne peut être null
+            if (data == null)
+                throw new ArgumentNullException("data argument cannot be null or empty.", "data");
+
+            //data ne peut être vide
+            if (data.Length == 0)
+                throw new ArgumentException("data argument cannot have a length of 0.", "data");
+
+            _data = data;
+        }
+    }
+}
