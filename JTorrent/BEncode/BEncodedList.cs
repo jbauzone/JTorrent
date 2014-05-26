c0d83af720f8f684a02120421028b5cf559d66a6
Jérémy Bauzone <bauzonejeremy@gmail.com>
2014-05-26 22:45:12 +0200

    Ajout des constructeurs sur BEncodedList

--- JTorrent/BEncode/BEncodedList.cs
+++ JTorrent/BEncode/BEncodedList.cs
@@ -13,6 +13,26 @@ namespace JTorrent.BEncode {
 
         public List<BEncodedValue> Value { get; set; }
 
+        public BEncodedList() : base() {
+            Value = new List<BEncodedValue>();
+        }
+        
+        /// <summary>
+        /// Initialise les données BEncode à décoder avec la chaine de caractères fournie
+        /// </summary>
+        /// <param name="data"></param>
+        public BEncodedList(string data) : base(data) {
+            Value = new List<BEncodedValue>();
+        }
+
+        /// <summary>
+        /// Initialise les données BEncode à décoder avec le tableau de byte fourni
+        /// </summary>
+        /// <param name="data"></param>
+        public BEncodedList(byte[] data) : base(data) {
+            Value = new List<BEncodedValue>();
+        }
+
         public override void Decode(Queue<byte> stack) {
             throw new NotImplementedException();
         }
