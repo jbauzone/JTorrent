a0562f44d69c272b442f7f155537b0284c332352
Jérémy Bauzone <bauzonejeremy@gmail.com>
2015-09-18 22:10:06 +0200

    test(torrent): mise à jour du fichier de test

--- JTorrent.Tests/Download/TorrentTest.cs
+++ JTorrent.Tests/Download/TorrentTest.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using JTorrent.Download;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JTorrent.Tests.Commons;
 
 namespace JTorrent.Tests.Download {
 
@@ -18,8 +19,8 @@ namespace JTorrent.Tests.Download {
         [TestMethod]
         public void LoadTorrentFromFile() {
 
-            Torrent torrent = new Torrent("pirates.torrent");
-            Assert.AreEqual(1231877749, torrent.CreationDate);
+            Torrent torrent = new Torrent(Tools.GetTestDataFilePath("ubuntu-15.04-desktop-amd64.iso.torrent"));
+            Assert.AreEqual(1429786237, torrent.CreationDate);
         }
 
         /// <summary>
@@ -28,8 +29,18 @@ namespace JTorrent.Tests.Download {
         [TestMethod]
         public void GetInfoHash() {
 
-            Torrent torrent = new Torrent("pirates.torrent");
-            Assert.AreEqual("3373240EC08A5F9F0AB1369B3E276D04970405AA", torrent.GetInfoHash());
+            Torrent torrent = new Torrent(Tools.GetTestDataFilePath("ubuntu-15.04-desktop-amd64.iso.torrent"));
+            Assert.AreEqual("FC8A15A2FAF2734DBB1DC5F7AFDC5C9BEAEB1F59", torrent.GetInfoHash());
+        }
+
+        /// <summary>
+        /// Vérifie le hash encodé d'un Torrent
+        /// </summary>
+        [TestMethod]
+        public void GetEncodedInfoHash() {
+
+            Torrent torrent = new Torrent(Tools.GetTestDataFilePath("ubuntu-15.04-desktop-amd64.iso.torrent"));
+            Assert.AreEqual("%fc%8a%15%a2%fa%f2sM%bb%1d%c5%f7%af%dc%5c%9b%ea%eb%1fY", torrent.GetEncodedInfoHash());
         }
     }
 }
