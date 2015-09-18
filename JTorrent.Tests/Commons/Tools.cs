7336a97f8f511873cb7a24e4c28727f09d700f1a
Jérémy Bauzone <bauzonejeremy@gmail.com>
2015-09-18 22:09:30 +0200

    test(torrent): ajout d'un fichier torrent de test et class pour le récupérer

---
+++ JTorrent.Tests/Commons/Tools.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JTorrent.Tests.Commons {
+
+    /// <summary>
+    /// Méthodes "outils" pour les tests
+    /// </summary>
+    static class Tools {
+
+        /// <summary>
+        /// Récupère le chemin absolu du fichier de test fourni 
+        /// </summary>
+        /// <param name="fileName">Fichier de test de Test_Date</param>
+        /// <returns>Chemin absolu vers le fichier de test</returns>
+        public static string GetTestDataFilePath(string fileName) {
+
+            string startupPath = AppDomain.CurrentDomain.BaseDirectory;
+            var pathItems = startupPath.Split(Path.DirectorySeparatorChar);
+            string projectPath = string.Join (Path.DirectorySeparatorChar.ToString(), pathItems.Take(pathItems.Length - 2));
+            return Path.Combine(projectPath, "Test_Data", fileName);
+        }
+    }
+}
