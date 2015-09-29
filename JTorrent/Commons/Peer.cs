56b7aa4d777e46fa526a91ccc49d28a237b1c066
Jérémy Bauzone <bauzonejeremy@gmail.com>
2015-09-29 21:07:45 +0200

    feat(peer): ajout de la classe représentant un Peer

---
+++ JTorrent/Commons/Peer.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+namespace JTorrent.Commons {
+
+    /// <summary>
+    /// Représente un Peer
+    /// </summary>
+    public class Peer {
+
+        public IPEndPoint IPEndPoint { get; set; }
+
+        /// <summary>
+        /// Construit un peer à partir de son adresse IP:port
+        /// </summary>
+        /// <param name="ipEndPoint">Adresse IP:port du peer</param>
+        public Peer(IPEndPoint ipEndPoint) {
+            IPEndPoint = ipEndPoint;
+        }
+    }
+}
